ccd072dd5de2c35c20d53390319d7d469319e18b
Sodja Nathan  O <sodjanathan@gmail.com>
2023-06-01 13:12:55 +0000

    chore: created server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('E-Commerce API');
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('App is ready');
+})
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is live, port: ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();

6af162f8d80a83299fcd6017b065b1313227cf43
Sodja Nathan  O <sodjanathan@gmail.com>
2023-06-28 02:11:39 +0000

    feat: create user acc

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,32 @@
-const logout = (req, res, next) => {
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+} = require("../errors");
+
+const logout = async (req, res, next) => {
   console.log("logout user");
   res.send("logout route");
 };
-const login = (req, res, next) => {
+
+const login = async (req, res, next) => {
   console.log("login user");
   res.send("login route");
 };
-const register = (req, res, next) => {
-  console.log("register user");
-  res.send("register route");
+
+const register = async (req, res, next) => {
+  const { email } = req.body;
+  const existingEmail = User.findOne({ email: email });
+
+  if (existingEmail) {
+    throw new BadRequestError("Email already exists");
+  }
+  
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).send(user);
 };
 
 module.exports = { register, login, logout };

08c0f7172f319c46e162e3a80221866fd369f8d2
Sodja Nathan  O <sodjanathan@gmail.com>
2023-09-05 11:39:26 +0000

    feat: aggregation for product done

--- models/Review.js
+++ models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { CustomAPIError } = require("../errors");
 
 const ReviewSchema = new mongoose.Schema(
   {
@@ -35,29 +36,30 @@ const ReviewSchema = new mongoose.Schema(
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true }); // means a user can post only one review to a product
 
 ReviewSchema.statics.calculateAverageRating = async function (productId) {
-  console.log(productId);
+  const aggregationResult = await this.aggregate([
+    { $match: { product: productId } }, // Match reviews for the specified product,
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" }, // Calculate the average of the 'rating' field
+        numberOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(aggregationResult[0]?.averageRating || 0),
+        numberOfReviews: aggregationResult[0]?.numberOfReviews || 0,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    throw new CustomAPIError("Couldn't find product");
+  }
 };
-
-// ReviewSchema.statics.calculateAverageRating = async function (productId) {
-//   const aggregationResult = await this.aggregate([
-//     {
-//       $match: { productId: productId }, // Match reviews for the specified product
-//     },
-//     {
-//       $group: {
-//         _id: null,
-//         averageRating: { $avg: "$rating" }, // Calculate the average of the 'rating' field
-//       },
-//     },
-//   ]);
-
-//   if (aggregationResult.length > 0) {
-//     const averageRating = aggregationResult[0].averageRating;
-//     return averageRating;
-//   } else {
-//     return 0; // No reviews found for the product
-//   }
-// };
 
 ReviewSchema.post("save", async function () {
   await this.constructor.calculateAverageRating(this.product);
